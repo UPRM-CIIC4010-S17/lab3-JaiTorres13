cf9b3ce8521e00571f03a3c82d4ac7b80271df58
JaiTorres13 <jainel.torres@upr.edu>
2017-11-16 16:27:34 -0400

    updated the JFrame, its name and location

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,17 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Jainel M. Torres Santos");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int) screenSize.getWidth(); // the screen width
+		int height = (int) screenSize.getHeight();// the screen height
+		myFrame.setLocation((width/2) -100, (height/2) -100); // set location to the center of the screen
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
