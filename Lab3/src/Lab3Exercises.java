8d0612622cdd42439c100d657c49ae3ec807a642
JaiTorres13 <jainel.torres@upr.edu>
2017-11-16 23:19:31 -0400

    added some lines, draw an oval, arrow and star

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,13 +5,19 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
+		
 		JFrame myFrame = new JFrame("Jainel M. Torres Santos");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int width = (int) screenSize.getWidth(); // the screen width
 		int height = (int) screenSize.getHeight();// the screen height
 		myFrame.setLocation((width/2) -100, (height/2) -100); // set location to the center of the screen
-		myFrame.setSize(200, 200);
-		myFrame.setVisible(true);
+		myFrame.setSize(250, 250);
+		MyPanelClass myPanel = new MyPanelClass();
+
+        myFrame.getContentPane().add(myPanel);
+        myFrame.setVisible(true);
+        
+       
 	}
 }
